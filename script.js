cf7e060b40892436debb68a74703a59d4efbd61a
preetha vaishnavi <vaishnavihiteash@gmail.com>
2026-03-16 20:30:36 +0530

    Image banner

--- script.js
+++ script.js
@@ -1,134 +1,29 @@
-const searchGame = document.querySelector("#search-game");
-const games = document.querySelector("#games");
-let page = 1;
-let category = "";
+let index = 0;
+let slides = document.getElementsByClassName("banner");
 
-//deafult game when web begin run
-const startPage = async () => {
-  try {
-    const url = `https://cs-steam-api.herokuapp.com/features`;
-    const res = await fetch(url);
-    const data = await res.json();
-    games.innerHTML = data.data
-      .map(
-        (item) => `<div class="tag-game">
-<div class="img-game">
-  <img
-    src="${item.header_image}"+
-    alt="${item.name}"
-  />
-</div>
-<div class="name-price-game">
-    <div class="name-game">${item.name}</div>
-    <div class="price-game">${item.price}</div>
-</div>
-</div>`
-      )
-      .join("");
-  } catch (err) {
-    console.log("err", err);
-  }
-};
-startPage();
+function showSlide(i){
 
-//get search game
-const getAllGame = async () => {
-  try {
-    const url = `https://cs-steam-api.herokuapp.com/games?limit=20&q=${searchGame.value}`;
-    const res = await fetch(url);
-    const data = await res.json();
+for(let s of slides){
+s.style.display = "none";
+}
 
-    games.innerHTML = data.data
-      .map(
-        (item) => `<div class="tag-game">
-<div class="img-game">
-  <img
-    src="${item.header_image}"+
-    alt="${item.name}"
-  />
-</div>
-<div class="name-price-game">
-    <div class="name-game">${item.name}</div>
-    <div class="price-game">${item.price}</div>
-</div>
-</div>`
-      )
-      .join("");
-  } catch (err) {
-    console.log("err", err);
-  }
-};
+slides[i].style.display = "block";
+}
 
-//search game when click on category lick
-const categoryGame = async (key) => {
-  console.log(key);
-  category = key;
-  getData();
-};
+function changeSlide(step){
+index += step;
 
-const getData = async () => {
-  let url = `https://cs-steam-api.herokuapp.com/games?page=${page}&limit=20`;
-  try {
-    if (category) {
-      url += `&steamspy_tags=${category}`;
-    }
+if(index >= slides.length){
+index = 0;
+}
 
-    const res = await fetch(url);
-    const data = await res.json();
+if(index < 0){
+index = slides.length - 1;
+}
 
-    games.innerHTML = data.data
-      .map(
-        (item) => `<div class="tag-game">
-<div class="img-game">
-  <img
-    src="${item.header_image}"+
-    alt="${item.name}"
-  />
-</div>
-<div class="name-price-game">
-    <div class="name-game">${item.name}</div>
-    <div class="price-game">${item.price}</div>
-</div>
-</div>`
-      )
-      .join("");
-  } catch (err) {
-    console.log("err", err);
-  }
-};
+showSlide(index);
+}
 
-//backward forward funticon
-
-const forward = async () => {
-  page += 1;
-  getData();
-};
-
-const backward = async () => {
-  if (page < 1) {
-    page = 1;
-  } else {
-    page -= 1;
-  }
-  getData();
-};
-
-//creat list category
-const genresList = async () => {
-  try {
-    const url = `https://cs-steam-api.herokuapp.com/steamspy-tags`;
-    const res = await fetch(url);
-    const data = await res.json();
-    let categoryElement = document.querySelector("#category-game");
-    categoryElement.innerHTML = data.data
-      .map(
-        (item) =>
-          `<div onclick="categoryGame('${item.name}')" class="filterGame">${item.name}</div>`
-      )
-      .join("");
-  } catch (err) {
-    console.log("err", err);
-  }
-};
-
-genresList();
+setInterval(function(){
+changeSlide(1);
+},3000);
