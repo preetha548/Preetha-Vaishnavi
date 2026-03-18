0775cc463ebd97581268db6cda5d48afbffd8f73
preetha vaishnavi <vaishnavihiteash@gmail.com>
2026-03-18 12:43:16 +0530

    Honda service coupon

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-document.getElementById("regForm").addEventListener("submit", function(e) {
-  e.preventDefault();
-
-  let childName = document.getElementById("childName").value;
-  let parentName = document.getElementById("parentName").value;
-  let email = document.getElementById("email").value;
-
-  if (childName === "" || parentName === "" || email === "") {
-    alert("Please fill all required fields!");
-    return;
-  }
-
-  document.getElementById("message").innerText = "Form submitted successfully!";
-  
-  document.getElementById("regForm").reset();
-});
+function bookNow() {
+  alert("Booking page coming soon!");
+}
