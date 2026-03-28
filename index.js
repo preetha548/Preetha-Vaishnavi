51d3d229e01daae1c627695027da7c50a9a79f0a
preetha vaishnavi <vaishnavihiteash@gmail.com>
2026-03-28 17:50:31 +0530

    Scissors

--- index.js
+++ index.js
@@ -1,45 +1,72 @@
-const listItems = document.querySelectorAll(".list-item");
-const lists = document.querySelectorAll(".list");
+function showScore() {
+  document.querySelector("#user1-score").innerHTML = user1_score;
+  document.querySelector("#user2-score").innerHTML = user2_score;
 
-let draggedItem = null;
-
-for (let i = 0; i < listItems.length; i++) {
-  const item = listItems[i];
-
-  item.addEventListener("dragstart", function () {
-    draggedItem = item;
-    setTimeout(() => {
-      item.style.display = "none";
-    }, 0);
-  });
-
-  item.addEventListener("dragend", function () {
-    setTimeout(() => {
-      draggedItem.style.display = "block";
-      draggedItem = null;
-    }, 0);
-  });
-
-  for (let j = 0; j < lists.length; j++) {
-    const list = lists[j];
-
-    list.addEventListener("dragover", function (e) {
-      e.preventDefault();
-    });
-
-    list.addEventListener("dragenter", function (e) {
-      e.preventDefault();
-      this.style.backgroundColor = "rgba(0, 0, 0, 0.2)";
-    });
-
-    list.addEventListener("dragleave", function (e) {
-      e.preventDefault();
-      this.style.backgroundColor = "rgba(0, 0, 0, 0.1)";
-    });
-
-    list.addEventListener("drop", function (e) {
-      this.append(draggedItem);
-      this.style.backgroundColor = "rgba(0, 0, 0, 0.1)";
-    });
+  if (count == 0) {
+    if (user1_score === user2_score) {
+      alert("It's a Draw!!!");
+    } else {
+      user1_score > user2_score ? alert("you win !!!") : alert("you lost");
+    }
+    window.location.reload();
   }
 }
+
+function setScore(user1, user2) {
+  if (user1 === user2) {
+    return;
+  } else {
+    if (user1 === "Rock") {
+      if (user2 === "Paper") {
+        user2_score += 1;
+      } else if (user2 === "Scissor") {
+        user1_score += 1;
+      } else {
+        return;
+      }
+    } else if (user1 === "Paper") {
+      if (user2 === "Rock") {
+        user1_score += 1;
+      } else if (user2 === "Scissor") {
+        user2_score += 1;
+      } else {
+        return;
+      }
+    } else if (user1 === "Scissor") {
+      if (user2 === "Paper") {
+        user1_score += 1;
+      } else if (user2 === "Rock") {
+        user2_score += 1;
+      } else {
+        return;
+      }
+    }
+  }
+  showScore();
+}
+
+function showValue(user1_value, user2_value) {
+  document.querySelector("#user").innerHTML = user1_value;
+  document.querySelector("#user2").innerHTML = user2_value;
+}
+
+function showCount() {
+  document.querySelector("h2").innerHTML = `${count} out of 5 chance left`;
+}
+
+function getValue(event) {
+  event.preventDefault();
+  let user1 = document.activeElement.getAttribute("value");
+  let values = ["Rock", "Paper", "Scissor"];
+  let random_value = Math.round(Math.random() * 2);
+  let user2 = values[random_value];
+  count -= 1;
+  showCount();
+  showValue(user1, user2);
+  setScore(user1, user2);
+}
+
+document.querySelector("form").addEventListener("submit", getValue);
+let user1_score = 0;
+let user2_score = 0;
+let count = 5;
